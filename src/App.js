bc5b724eafaa300f4f23c128e8e685cbc1bc4965
corderoangel <corderoan777@gmail.com>
2024-10-04 08:29:04 -0500

    completando y eliminando todos

--- src/App.js
+++ src/App.js
@@ -26,7 +26,19 @@ function App() {
 		return todoText.includes(searchText);
 	});
 
-	console.log("Los usuarios buscan todos de " + searchValue);
+	const completeTodo = (text) => {
+		const newTodos = [...todos];
+		const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+		newTodos[todoIndex].completed = true;
+		setTodos(newTodos);
+	};
+
+	const deleteTodo = (text) => {
+		const newTodos = [...todos];
+		const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+		newTodos.splice(todoIndex, 1);
+		setTodos(newTodos);
+	};
 
 	return (
 		<>
@@ -35,7 +47,7 @@ function App() {
 
 			<TodoList>
 				{searchedTodos.map((todo) => (
-					<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+					<TodoItem key={todo.text} text={todo.text} completed={todo.completed} onComplete={() => completeTodo(todo.text)} onDelete={() => deleteTodo(todo.text)} />
 				))}
 			</TodoList>
 
