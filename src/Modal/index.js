ec26dd96d1b6d4fe9c42057afa866282d23c5803
corderoangel <corderoan777@gmail.com>
2024-10-10 14:41:18 -0500

    formularios

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./Modal.css";
 
 function Modal({ children }) {
-	return ReactDOM.createPortal(<div className="Modal">{children}</div>, document.getElementById("modal"));
+	return ReactDOM.createPortal(<div className="ModalBackground">{children}</div>, document.getElementById("modal"));
 }
 
 export { Modal };
--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -6,7 +6,7 @@ const TodoContext = React.createContext();
 function TodoProvider({ children }) {
 	const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage("TODOS_V1", []);
 	const [searchValue, setSearchValue] = React.useState("");
-	const [openModal, setOpenModal] = React.useState(true);
+	const [openModal, setOpenModal] = React.useState(false);
 
 	const completedTodos = todos.filter((todo) => !!todo.completed).length;
 	const totalTodos = todos.length;
@@ -16,6 +16,15 @@ function TodoProvider({ children }) {
 		const searchText = searchValue.toLowerCase();
 		return todoText.includes(searchText);
 	});
+
+	const addTodo = (text) => {
+		const newTodos = [...todos];
+		newTodos.push({
+			text,
+			completed: false,
+		});
+		saveTodos(newTodos);
+	};
 
 	const completeTodo = (text) => {
 		const newTodos = [...todos];
@@ -41,6 +50,7 @@ function TodoProvider({ children }) {
 				searchValue,
 				setSearchValue,
 				searchedTodos,
+				addTodo,
 				completeTodo,
 				deleteTodo,
 				openModal,
